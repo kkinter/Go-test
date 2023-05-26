a3d19f8961cde36b2a8e7803a7d045314df626b3
kkinter <ifol1129@gmail.com>
2023-05-26 14:46:16 +0900

    Feat: middleware and test

--- webapptest/cmd/web/handlers_test.go
+++ webapptest/cmd/web/handlers_test.go
@@ -2,21 +2,26 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"strings"
 	"testing"
 )
 
 func Test_application_handlers(t *testing.T) {
 	var handlerTests = []struct {
-		name               string
-		url                string
-		expectedStatusCode int
+		name                    string
+		url                     string
+		expectedStatusCode      int
+		expectedURL             string
+		expectedFirstStatusCode int
 	}{
-		{"home", "/", http.StatusOK},
-		{"404", "/fish", http.StatusNotFound},
+		{"home", "/", http.StatusOK, "/", http.StatusOK},
+		{"404", "/fish", http.StatusNotFound, "/fish", http.StatusNotFound},
+		{"profile", "/user/profile", http.StatusOK, "/", http.StatusTemporaryRedirect},
 	}
 
 	routes := app.routes()
@@ -24,6 +29,17 @@ func Test_application_handlers(t *testing.T) {
 	// create a test server
 	ts := httptest.NewTLSServer(routes)
 	defer ts.Close()
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	client := &http.Client{
+		Transport: tr,
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
 
 	for _, e := range handlerTests {
 		resp, err := ts.Client().Get(ts.URL + e.url)
@@ -35,6 +51,15 @@ func Test_application_handlers(t *testing.T) {
 		if resp.StatusCode != e.expectedStatusCode {
 			t.Errorf("%s: expected status %d, but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
 		}
+
+		if resp.Request.URL.Path != e.expectedURL {
+			t.Errorf("%s: want final URL %s but got %s", e.name, e.expectedURL, resp.Request.URL.Path)
+		}
+
+		resp2, _ := client.Get(ts.URL + e.url)
+		if resp2.StatusCode != e.expectedFirstStatusCode {
+			t.Errorf("%s: want first returned statud code to be %d but got %d", e.name, e.expectedFirstStatusCode, resp2.StatusCode)
+		}
 	}
 }
 
@@ -45,7 +70,7 @@ func TestAppHome(t *testing.T) {
 		wantHTML     string
 	}{
 		{"first visit", "", "<small>From Session"},
-		{"second visit", "hello, world!", "<small>From Session hello, world!"},
+		{"second visit", "hello, world!", "<small>From Session: hello, world!"},
 	}
 
 	for _, e := range tests {
@@ -104,4 +129,78 @@ func addContextAndSessionToRequest(req *http.Request, app application) *http.Req
 	ctx, _ := app.Session.Load(req.Context(), req.Header.Get("X-Session"))
 
 	return req.WithContext(ctx)
+}
+
+func Test_app_Login(t *testing.T) {
+	var tests = []struct {
+		name           string
+		postedData     url.Values
+		wantStatusCode int
+		wantLoc        string
+	}{
+		{
+			name: "valid login",
+			postedData: url.Values{
+				"email":    {"admin@example.com"},
+				"password": {"secret"},
+			},
+			wantStatusCode: http.StatusSeeOther,
+			wantLoc:        "/user/profile",
+		},
+		{
+			name: "missing form data",
+			postedData: url.Values{
+				"email":    {""},
+				"password": {""},
+			},
+			wantStatusCode: http.StatusSeeOther,
+			wantLoc:        "/",
+		},
+		{
+			name: "user not found",
+			postedData: url.Values{
+				"email":    {"me@example.com"},
+				"password": {"password"},
+			},
+			wantStatusCode: http.StatusSeeOther,
+			wantLoc:        "/",
+		},
+		{
+			name: "bad credentials",
+			postedData: url.Values{
+				"email":    {"admin@example.com"},
+				"password": {"password"},
+			},
+			wantStatusCode: http.StatusSeeOther,
+			wantLoc:        "/",
+		},
+	}
+
+	for _, e := range tests {
+		req, _ := http.NewRequest("POST", "/login", strings.NewReader(e.postedData.Encode()))
+		req = addContextAndSessionToRequest(req, app)
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		rr := httptest.NewRecorder()
+
+		handeler := http.HandlerFunc(app.Login)
+		handeler.ServeHTTP(rr, req)
+
+		if rr.Code != e.wantStatusCode {
+			t.Errorf("%s: wrong statuscode want %d but got %d", e.name, e.wantStatusCode, rr.Code)
+		}
+
+		actualLoc, err := rr.Result().Location()
+		if err == nil {
+			if actualLoc.String() != e.wantLoc {
+				t.Errorf("%s: want %s but got %s", e.name, e.wantLoc, actualLoc)
+			}
+
+			// === RUN   Test_app_Login
+			//			handlers_test.go:142: /user/profile
+			// t.Log(actualLoc.String())
+
+		} else {
+			t.Errorf("%s: no location header set", e.name)
+		}
+	}
 }
