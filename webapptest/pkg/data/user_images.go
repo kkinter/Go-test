f5eeccec416da2456fb01e02b112a26ff61cd47d
kkinter <ifol1129@gmail.com>
2023-05-26 16:57:28 +0900

    Feat: DB integration test

--- webapptest/pkg/data/user_images.go
+++ webapptest/pkg/data/user_images.go
@@ -5,7 +5,7 @@ import "time"
 // UserImage는 사용자 프로필 이미지의 유형입니다.
 type UserImage struct {
 	ID        int       `json:"id"`
-	UserID    string    `json:"user_id"`
+	UserID    int       `json:"user_id"`
 	FileName  string    `json:"file_name"`
 	CreatedAt time.Time `json:"-"`
 	UpdatedAt time.Time `json:"-"`
